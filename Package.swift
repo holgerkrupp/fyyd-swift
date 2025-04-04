d5bead70ce8c72a9da7140574c745f3614352b73
Holger Krupp <email@holgerkrupp.de>
2025-04-04 21:17:27 +0200

    error: the replacement path doesn't exist: "/var/folders/7x/fjrdgpw14pg9gbcmffz4ltg80000gn/T/swift-generated-sources/@__swiftmacro_4Raul15EpisodeListViewV8episodes33_5EDE931B24BB03A6ECB0C989C4FCC068LL5QueryfMa_.swift"

--- Package.swift
+++ Package.swift
@@ -4,21 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "ffyd_swift",
+    name: "fyyd_swift",
     platforms: [
         .iOS(.v18)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "ffyd_swift",
-            targets: ["ffyd_swift"]),
+            name: "fyyd_swift",
+            targets: ["fyyd_swift"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ffyd_swift"),
+            name: "fyyd_swift")
 
     ]
 )
